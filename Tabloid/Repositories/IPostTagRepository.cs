6fc5932e20a74622b77b23fb8df43aa594409f88
Alex Bishop <alexb98@gmail.com>
2023-07-18 09:33:42 -0500

    Post tags now deletable by author

--- Tabloid/Repositories/IPostTagRepository.cs
+++ Tabloid/Repositories/IPostTagRepository.cs
@@ -6,6 +6,7 @@ namespace Tabloid.Repositories
     public interface IPostTagRepository
     {
         void AddPostTag(PostTag postTag);
+        void DeletePostTag(int id);
         List<PostTag> GetAll();
         List<PostTag> GetAllByPostId(int id);
     }
